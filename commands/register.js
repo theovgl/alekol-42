455bc98f5150bac77ff849a4d0337da320fe89ed
vfurmane <vfurmane@student.42.fr>
2022-02-06 11:18:44 +0100

    feat: register command replies with embedded message

--- commands/register.js
+++ commands/register.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
 const supabase = require('../utils/supabase.js');
 const ft_api = require('../utils/ft_api.js');
 const users = require('../src/users.js');
@@ -18,6 +19,11 @@ module.exports = {
 			await interaction.editReply('😵 An unknown error occurred... Please try again later!');
 			return;
 		}
-		await interaction.editReply(`Follow the link dumb bitch\nhttps://api.intra.42.fr/oauth/authorize?client_id=137cb6a1ee4053050015749731fd55dcfe71a54cfce122a5ac42696e428a0c8d&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&state=${state}`);
+		const embed = new MessageEmbed()
+			.setColor('#1abc9c')
+			.setTitle('Registration request')
+			.setDescription('Please follow the link')
+			.setURL(`https://api.intra.42.fr/oauth/authorize?client_id=137cb6a1ee4053050015749731fd55dcfe71a54cfce122a5ac42696e428a0c8d&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&state=${state}`);
+		await interaction.editReply({ embeds: [embed] });
 	},
 };
