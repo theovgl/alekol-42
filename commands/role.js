128ebd0599e2cd3285217ad766751cf4096d0541
vfurmane <vfurmane@student.42.fr>
2022-02-09 15:30:46 +0100

    feat: create the role if it does not exist

--- commands/role.js
+++ commands/role.js
@@ -17,10 +17,8 @@ module.exports = {
 			await supabase.setGuildRole(interaction.guildId, interaction.applicationId, role_name);
 			await interaction.guild.members.fetch();
 			const role_manager = interaction.guild.roles.cache.find(role => role.name == guild_data[0].role);
-			const new_role_manager = interaction.guild.roles.cache.find(role => role.name == role_name);
-			interaction.guild.members.cache.forEach((member) => {
-				console.log(member.displayName);
-			});
+			let new_role_manager = interaction.guild.roles.cache.find(role => role.name == role_name);
+			if (!new_role_manager) new_role_manager = await interaction.guild.roles.create({ name: role_name });
 			role_manager.members.forEach((member) => {
 				member.roles.remove(role_manager);
 				member.roles.add(new_role_manager);
