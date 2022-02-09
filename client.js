36a75ef8cdb6e12145d91cd6511f9e8333496fb7
vfurmane <vfurmane@student.42.fr>
2022-02-09 15:12:53 +0100

    feat: change the members' role when changing the guild's role

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
 const supabase = require('./utils/supabase.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
 client.commands = new Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
--- commands/role.js
+++ commands/role.js
@@ -11,9 +11,20 @@ module.exports = {
 				.setRequired(true)),
 	async execute(interaction) {
 		await interaction.deferReply({ ephemeral: true });
-		const role = interaction.options.getString('name');
+		const role_name = interaction.options.getString('name');
 		try {
-			await supabase.setGuildRole(interaction.guild.id, interaction.applicationId, role);
+			const guild_data = await supabase.fetchGuild(interaction.guildId, interaction.applicationId);
+			await supabase.setGuildRole(interaction.guildId, interaction.applicationId, role_name);
+			await interaction.guild.members.fetch();
+			const role_manager = interaction.guild.roles.cache.find(role => role.name == guild_data[0].role);
+			const new_role_manager = interaction.guild.roles.cache.find(role => role.name == role_name);
+			interaction.guild.members.cache.forEach((member) => {
+				console.log(member.displayName);
+			});
+			role_manager.members.forEach((member) => {
+				member.roles.remove(role_manager);
+				member.roles.add(new_role_manager);
+			});
 		} catch (error) {
 			console.error(error);
 			await interaction.editReply('😵 An unknown error occurred... Please try again later!');
