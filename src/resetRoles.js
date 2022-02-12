50695970564aed880ed5369c78b13058f8608074
vfurmane <vfurmane@student.42.fr>
2022-02-12 16:03:29 +0100

    fix: check if the role was found in the guild

--- src/resetRoles.js
+++ src/resetRoles.js
@@ -9,6 +9,10 @@ async function resetRoles(supabase, discord) {
 		const guild_data = await supabase.fetchGuild(guild.id, discord.application.id)
 		await guild.members.fetch();
 		const role_manager = guild.roles.cache.find((role) => role.name == guild_data[0].role);
+		if (!role_manager) {
+			console.error(`The role ${guild_data[0].role} has not been found in guild ${guild.name}`);
+			return;
+		}
 		for (const member of role_manager.members.values()) {
 			member.roles.remove(role_manager);
 		};
