c8efbfcf50b4b073b5a770bd816c5b2bbdd20702
vfurmane <vfurmane@student.42.fr>
2022-02-15 11:28:28 +0100

    refactor: eslint

--- src/resetRoles.js
+++ src/resetRoles.js
@@ -2,22 +2,22 @@ async function resetRoles(supabase, discord) {
 	const requests = [];
 	for (const guild of discord.guilds.cache.values()) {
 		requests.push(supabase.fetchGuild(guild.id, discord.application.id)
-				.then(async (guild_data) => {
-					await guild.members.fetch();
-					const role_manager = guild.roles.cache.find((role) => role.name == guild_data[0].role);
-					if (!role_manager) {
-						console.error(`The role ${guild_data[0].role} has not been found in guild ${guild.name}`);
-						return;
-					}
-					const roles_requests = [];
-					for (const member of role_manager.members.values()) {
-						roles_requests.push(member.roles.remove(role_manager));
-					}
-					await Promise.all(roles_requests);
-				})
-				.catch((error) => {
-					console.error(error);
-				})
+			.then(async (guild_data) => {
+				await guild.members.fetch();
+				const role_manager = guild.roles.cache.find((role) => role.name == guild_data[0].role);
+				if (!role_manager) {
+					console.error(`The role ${guild_data[0].role} has not been found in guild ${guild.name}`);
+					return;
+				}
+				const roles_requests = [];
+				for (const member of role_manager.members.values()) {
+					roles_requests.push(member.roles.remove(role_manager));
+				}
+				await Promise.all(roles_requests);
+			})
+			.catch((error) => {
+				console.error(error);
+			}),
 		);
 	}
 	await Promise.all(requests);
