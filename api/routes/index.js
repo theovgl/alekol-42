0f9492f91966705152c9ad4feb132eda4ade986a
vfurmane <vfurmane@student.42.fr>
2022-04-11 12:36:04 +0200

    test: add tests for the new API

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { logAction } = require('../../src/logs.js')
+const { logAction } = require('../../src/logs.js');
 const registerRoute = require('./register');
 
 module.exports = (discord) => {
@@ -7,18 +7,18 @@ module.exports = (discord) => {
 	const route = express.Router();
 
 	route.use((req, res, next) => {
-	    express.json()(req, res, err => {
-	        if (err) {
+		express.json()(req, res, err => {
+			if (err) {
 				logAction(console.error, 'An error occured while parsing the request\'s JSON');
-	            console.error(err);
+				console.error(err);
 				return res.status(400).json({
 					message: 'The request is incorrect...',
-					details: 'The body is not JSON formatted.'
+					details: 'The body is not JSON formatted.',
 				});
-	        }
-	
-	        next();
-	    });
+			}
+
+			next();
+		});
 	});
 	route.use(express.json());
 	route.use('/register', registerRoute(discord));
--- api/routes/register/index.js
+++ api/routes/register/index.js
@@ -15,29 +15,29 @@ async function ft_registration(state_data, code) {
 	state_data.ft_login = ft_user.login;
 	return {
 		service: 'Discord',
-		location: `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI + '/register')}&response_type=code&scope=identify&state=${state_data.state}`
+		location: `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI + '/register')}&response_type=code&scope=identify&state=${state_data.state}`,
 	};
 }
 
-async function discord_registration(state_data, code) {
+async function discord_registration(discord, state_data, code) {
 	const discord_user = await discord_api.fetchMe(code)
 		.catch(async (error) => {
 			await supabase.deleteState(state_data.state);
 			throw { reason: error, message: 'The given code does not allow to fetch the user\'s data.', httpCode: 400 };
 		});
+	await supabase.deleteState(state_data.state);
 	if (await supabase.userExists(discord_user.id, state_data.ft_login, state_data.guild_id)) return null;
 	// Insert the user in the database
 	await supabase.insertUser(discord_user.id, state_data.ft_login, state_data.guild_id);
-	await supabase.deleteState(state_data.state);
 	// Update the user in the binary tree
 	const user = users.find(state_data.ft_login)?.data;
-	if (user) {
-		const guild_member = await discord.guilds.cache.get(state_data.guild_id)
-			.members.fetch(discord_user.id);
-		user.guilds_member.push(guild_member);
-		// Update the user's role according to its location
-		await user.updateRole();
-	}
+	if (user == null) return null;
+	const guild = discord.guilds.cache.get(state_data.guild_id);
+	if (guild == null) return null;
+	const guild_member = await guild.members.fetch(discord_user.id);
+	user.guilds_member.push(guild_member);
+	// Update the user's role according to its location
+	await user.updateRole();
 	return null;
 }
 
@@ -55,7 +55,7 @@ module.exports = (discord) => {
 			state_data = await supabase.fetchState(state);
 			if (state_data == null) throw { reason: new Error('The state was not found in the database.'), message: 'The state was not found in the database.', httpCode: 400 };
 			if (state_data.ft_login == null) next_step = await ft_registration(state_data, code);
-			else next_step = await discord_registration(state_data, code);
+			else next_step = await discord_registration(discord, state_data, code);
 		} catch (error) {
 			let message;
 			if (error?.httpCode == 400) message = 'The request is incorrect...';
@@ -63,7 +63,7 @@ module.exports = (discord) => {
 			console.error(error?.reason || error);
 			return res.status(error?.httpCode || 500).json({
 				message,
-				details: error?.httpCode ? error?.message : 'Please contact an administrator.'
+				details: error?.httpCode ? error?.message : 'Please contact an administrator.',
 			});
 		}
 		return res.json({
@@ -74,4 +74,4 @@ module.exports = (discord) => {
 
 	return route;
 
-}
+};
