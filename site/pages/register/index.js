822294a6b244bc8bea1cdc913d4cb1091a12d40b
tvogel <tvogel@student.42.fr>
2022-04-11 21:46:36 +0200

    wip

--- site/pages/register/index.js
+++ site/pages/register/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head';
 import Header from '../../components/Header';
 import StatusCard from '../../components/StatusCard';
 import styled from 'styled-components';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 const Container = styled.div`
 	display: flex;
@@ -10,14 +13,28 @@ const Container = styled.div`
 `;
 
 export default function index() {
+	let [status, setStatus] = useState(0);
+	const router = useRouter();
+	const { code } = router.query;
+	const { state } = router.query;
+
+	useEffect(() => {
+		axios.post('https://jsonplaceholder.typicode.com/users')
+			.then(res => {
+				setStatus(res.status);
+				console.log(status);
+			})
+	})
+
 	return (
 		<>
 			<Head>
 				<title>Alekol Registration</title>
+				<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👀</text></svg>" />
 			</Head>
 			<Header title='👀 Alekol Registration'/>
 			<Container>
-				<StatusCard status='failure'/>
+				<StatusCard code={status} />
 			</Container>
 		</>
 	)
