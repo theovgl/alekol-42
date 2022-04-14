78a6b02a6d2e8f1db761734d0b80d004d8314afe
tvogel <tvogel@student.42.fr>
2022-04-14 19:06:15 +0200

    feat: add robots.txt and crawling options

--- apps/site/pages/register/index.js
+++ apps/site/pages/register/index.js
@@ -59,15 +59,11 @@ export async function getServerSideProps(context) {
 }
 
 export default function index({ status, data }) {
-	useEffect(() => {
-		console.log(data);
-	});
-
 	return (
 		<>
 			<Head>
 				<title>Alekol Registration</title>
-				<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👀</text></svg>" />
+				<meta name='robots' content='noindex' />
 			</Head>
 			<Container>
 				<Header title='Alekol Registration'/>
