4aa647193621d128c055d2f550fd3d2d3048f7eb
theovgl <theo.vgl@outlook.com>
2022-04-16 17:06:04 +0200

    fix: change image link

--- apps/site/pages/_document.js
+++ apps/site/pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
 				<meta property="og:type" content="website" />
 				<meta property="og:description" content="Alekol is a Discord bot to automatically add or remove a role whether you are logged at 42 school." />
 				<meta property="og:url" content="https://alekol.42group.fr/" />
-				<meta property="og:image" content="https://i.ibb.co/ydr94Nk/og-image.png" />
+				<meta property="og:image" content="https://raw.githubusercontent.com/theovgl/alekol-42/opengraph/apps/site/public/og_image.png?token=GHSAT0AAAAAABTDX5Z76F35BW4PBZXV5EGWYS25ESA" />
 				<link rel='icon' href='/favicon.svg'/>
 				<link rel='preload' href='/fonts/Inter/static/Inter-Black.ttf' as='font' crossOrigin='' type='font/ttf'/>
 				<link rel='preload' href='/fonts/Inter/static/Inter-Regular.ttf' as='font' crossOrigin='' type='font/ttf'/>
