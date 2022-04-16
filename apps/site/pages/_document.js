c93397179d441e69a831db0e742c19ab6dd8c9aa
theovgl <theo.vgl@outlook.com>
2022-04-16 17:02:24 +0200

    feat: add openGraph informations

--- apps/site/pages/_document.js
+++ apps/site/pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<meta property="og:title" content="Alekol" />
+				<meta property="og:type" content="website" />
+				<meta property="og:description" content="Alekol is a Discord bot to automatically add or remove a role whether you are logged at 42 school." />
+				<meta property="og:url" content="https://alekol.42group.fr/" />
+				<meta property="og:image" content="https://i.ibb.co/ydr94Nk/og-image.png" />
 				<link rel='icon' href='/favicon.svg'/>
 				<link rel='preload' href='/fonts/Inter/static/Inter-Black.ttf' as='font' crossOrigin='' type='font/ttf'/>
 				<link rel='preload' href='/fonts/Inter/static/Inter-Regular.ttf' as='font' crossOrigin='' type='font/ttf'/>
